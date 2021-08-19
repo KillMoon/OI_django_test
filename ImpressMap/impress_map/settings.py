e0018e48b741f9ad5325443f1c4b35aef148b4f8
Artem <45537773+KillMoon@users.noreply.github.com>
2021-08-19 12:56:03 +0700

    Update settings.py

--- ImpressMap/impress_map/settings.py
+++ ImpressMap/impress_map/settings.py
@@ -200,4 +200,4 @@ ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # test
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # test123
